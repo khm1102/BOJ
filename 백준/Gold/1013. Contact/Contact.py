a612d939236613a01ab53623cdc4a4f1498da65a
Kim Hyunmin <hmkim112@naver.com>
2023-09-01 00:33:38 +0900

    [Gold V] Title: Contact, Time: 204 ms, Memory: 34804 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.py
+++ 백준/Gold/1013. Contact/Contact.py
@@ -1,14 +1,11 @@
 import re
 
-def check_pattern(input_str):
+def f(input_str):
     pattern = r'(100+1+|01)+'
     return re.fullmatch(pattern, input_str) is not None
 
 T = int(input())
-
 for _ in range(T):
-    test_str = input()
-    if check_pattern(test_str):
-        print("YES")
-    else:
-        print("NO")
+    str = input()
+    if f(str):print("YES")
+    else:print("NO")
