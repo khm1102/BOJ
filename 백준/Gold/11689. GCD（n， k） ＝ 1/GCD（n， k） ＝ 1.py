59df6c93fdbd9b9854202d0df55eccefb6e1dae3
Kim Hyunmin <hmkim112@naver.com>
2023-09-06 02:00:47 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,17 @@
+def f(n):
+    result = n
+    p = 2
+
+    while p * p <= n:
+        if n % p == 0:
+            while n % p == 0:
+                n //= p
+            result -= result // p
+        p += 1
+
+    if n > 1:
+        result -= result // n
+
+    return result
+
+print(f(int(input())))
