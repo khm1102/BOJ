3d92109b16136281dedc95673fdd36f574e640bb
Kim Hyunmin <hmkim112@naver.com>
2023-09-06 01:52:02 +0900

    [Silver IV] Title: 다음 소수, Time: 332 ms, Memory: 116268 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2 or num == 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+N = int(input())
+for _ in range(N):
+    n = int(input())
+    while not is_prime(n):
+        n += 1
+    print(n)
+
