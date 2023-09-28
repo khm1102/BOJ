8701ff5f899925c6b93455bde35c33562da2261c
Kim Hyunmin <hmkim112@naver.com>
2023-09-28 20:42:43 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
@@ -1,33 +1,14 @@
-def gauss_elimination(matrix):
+def gauss(matrix):
     n = len(matrix)
-
     for i in range(n):
-        div = matrix[i][i]
-        for j in range(i, n + 1):
-            matrix[i][j] /= div
-        
+        div = matrix[i][i];
+        for j in range(i, n + 1):matrix[i][j] /= div
         for j in range(n):
-            if i == j:
-                continue
+            if i == j:continue
             div = -matrix[j][i]
-            for k in range(n + 1):
-                matrix[j][k] += matrix[i][k] * div
-    
-    result = [row[n] for row in matrix]
-    return result
-
-def main():
-    n = int(input())
-    matrix = []
-    
-    for i in range(n):
-        row = list(map(float, input().split()))
-        matrix.append(row)
-    
-    result = gauss_elimination(matrix)
-    
-    for i in result:
-        print(f'{i:.0f}', end=' ')
-
-if __name__ == "__main__":
-    main()
+            for k in range(n + 1):matrix[j][k] += matrix[i][k] * div
+    return [row[n] for row in matrix]
+arr = []
+[arr.append(list(map(float,input().split()))) for i in range(int(input()))]
+res = (gauss(arr))
+print(*[f'{i:.0f}'for i in res])
