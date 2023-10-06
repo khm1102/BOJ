07bf52d6e29a1166f5edbeef6e8ab0874a2c2c98
Kim Hyunmin <hmkim112@naver.com>
2023-10-06 19:34:33 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.py
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.py
@@ -1,16 +1,11 @@
-def function(x):
-    sum_value = 0
+def f(x):
+    value = 0
     i = 1
-
     while x > 0:
         y = (x + 1) // 2
-        sum_value += y * i
+        value += y * i
         x //= 2
         i *= 2
-
-    return sum_value
-
-
-A, B = map(int, input().split())
-result = function(B) - function(A - 1)
-print(result)
+    return value
+a, b = map(int, input().split())
+print(f(b) - f(a - 1))
