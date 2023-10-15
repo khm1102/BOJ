a6bd6a9ffbcc87609bdc2d7697de8696ca04b90b
rlagusals1102 <hmkim112@naver.com>
2023-10-15 15:19:16 +0900

    [Bronze I] Title: 다각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11896. 다각형/다각형.py
+++ 백준/Bronze/11896. 다각형/다각형.py
@@ -2,11 +2,7 @@ import sys
 def input(): return sys.stdin.readline().strip()
 def print(val):return sys.stdout.write(str(val))
 
-a, b = map(lambda x: x if x > 2 else 3, map(int, input().split()))
-
-a = (a + 1) if a % 2 != 0 else a
-b = (b - 1) if b % 2 != 0 else b
-a //= 2
-b //= 2
-
-print(b * (b + 1) - a * (a - 1) if a <= b else 0)
+a, b = map(lambda x: max(x, 3), map(int, input().split()))
+a, b = (a + 1) if a % 2 else a, (b - 1) if b % 2 else b
+a, b = a // 2, b // 2
+print((b * (b + 1) - a * (a - 1)) if a <= b else 0)
