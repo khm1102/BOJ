77f41895f32a7a37eb88a92db11bc79cebb7eb36
rlagusals1102 <hmkim112@naver.com>
2023-10-25 02:48:40 +0900

    [Silver V] Title: 소트인사이드, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,11 +1,1 @@
-n = int(input())
- 
-li = []
-for i in str(n):
-    li.append(int(i))
-
-    
-li.sort(reverse=True) 
- 
-for i in li:
-    print(i,end='')
+print(''.join(map(str, sorted([int(i) for i in str(input())], reverse=True))))
