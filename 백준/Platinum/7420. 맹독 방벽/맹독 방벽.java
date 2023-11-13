987736a5a1e7e717fa3e44f8fbd3faa3d58db966
rlagusals1102 <hmkim112@naver.com>
2023-11-14 01:33:47 +0900

    [Platinum IV] Title: 맹독 방벽, Time: 292 ms, Memory: 21604 KB -BaekjoonHub

---
+++ 백준/Platinum/7420. 맹독 방벽/맹독 방벽.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int l = scanner.nextInt();
+
+        Point[] buildings = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            buildings[i] = new Point(x, y);
+        }
+
+        Point[] convexHull = monotoneChain(buildings);
+        double s = 0;
+
+        for (int i = 0; i < convexHull.length; i++) {
+            int prevIndex = (i == 0) ? convexHull.length - 1 : i - 1;
+            s += getDist(convexHull[prevIndex], convexHull[i]);
+        }
+
+        s += 2 * Math.PI * l;
+        System.out.println(round(s));
+    }
+
+    static int round(double n) {
+        return (int) ((n * 10 + 5) / 10);
+    }
+
+    static double ccw(Point p1, Point p2, Point p3) {
+        return p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y);
+    }
+
+    static Point[] monotoneChain(Point[] dots) {
+        Arrays.sort(dots);
+        if (dots.length <= 1) {
+            return dots;
+        }
+
+        ArrayList<Point> lower = new ArrayList<>();
+        for (Point d : dots) {
+            while (lower.size() >= 2 && ccw(lower.get(lower.size() - 2), lower.get(lower.size() - 1), d) <= 0) {
+                lower.remove(lower.size() - 1);
+            }
+            lower.add(d);
+        }
+
+        ArrayList<Point> upper = new ArrayList<>();
+        for (int i = dots.length - 1; i >= 0; i--) {
+            Point d = dots[i];
+            while (upper.size() >= 2 && ccw(upper.get(upper.size() - 2), upper.get(upper.size() - 1), d) <= 0) {
+                upper.remove(upper.size() - 1);
+            }
+            upper.add(d);
+        }
+
+        lower.remove(lower.size() - 1);
+        upper.remove(upper.size() - 1);
+        lower.addAll(upper);
+
+        return lower.toArray(new Point[0]);
+    }
+
+    static double getDist(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    static class Point implements Comparable<Point> {
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point other) {
+            if (this.x == other.x) {
+                return Integer.compare(this.y, other.y);
+            }
+            return Integer.compare(this.x, other.x);
+        }
+    }
+}
