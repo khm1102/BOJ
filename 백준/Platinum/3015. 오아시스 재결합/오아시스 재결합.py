2baff7a898c5dcbaa65e8d30d04c88a7e72ac8ff
rlagusals1102 <hmkim112@naver.com>
2023-12-02 17:40:47 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 328 ms, Memory: 114220 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -1,23 +1,19 @@
 from collections import deque
 import sys
 def input(): return sys.stdin.readline().strip()
-def print(val):return sys.stdout.write(str(val))
+def print(s):return sys.stdout.write(str(s))
 
-
-n = int(input())
 S = deque()
 ans = 0
 
-for _ in range(n):
+for _ in range(int(input())):
     h = int(input())
     cnt = 1
     while S and S[-1][0] <= h:
         ans += S[-1][1]
-        if S[-1][0] == h:
-            cnt += S[-1][1]
+        if S[-1][0] == h:cnt += S[-1][1]
         S.pop()
-    if S:
-        ans += 1
+    if S:ans += 1
     S.append((h, cnt))
 
 print(ans)
