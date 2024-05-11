330f26b7bf8d6d49681bf8a9a3a642bd476be56f
rlagusals1102 <hmkim112@naver.com>
2024-05-11 22:23:47 +0900

    [level 4] Title: 사칙연산, Time: 202.15 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,31 +1,32 @@
 def solution(arr):
+    n = len(arr) // 2
+    dp = [[[None for _ in range(n + 1)] for _ in range(n + 1)] for _ in range(2)]
+    num = [int(arr[i]) for i in range(0, len(arr), 2)]
+    ops = [arr[i] for i in range(1, len(arr), 2)]
+
     def calculate(flag, start, end):
         if start == end:
-            dp[flag][start][end] = numbers[start]
-            return dp[flag][start][end]
+            return num[start]
 
         if dp[flag][start][end] is not None:
             return dp[flag][start][end]
 
-        result = float('-inf') if flag == 0 else float('inf')
+        res = float('-inf') if flag == 0 else float('inf')
         for mid in range(start, end):
-            if operations[mid] == "-":
+            if ops[mid] == "-":
                 if flag == 0:
-                    result = max(result, calculate(0, start, mid) - calculate(1, mid + 1, end))
+                    res = max(res, calculate(0, start, mid) - calculate(1, mid + 1, end))
+
                 else:
-                    result = min(result, calculate(1, start, mid) - calculate(0, mid + 1, end))
+                    res = min(res, calculate(1, start, mid) - calculate(0, mid + 1, end))
             else:
                 if flag == 0:
-                    result = max(result, calculate(0, start, mid) + calculate(0, mid + 1, end))
+                    res = max(res, calculate(0, start, mid) + calculate(0, mid + 1, end))
+
                 else:
-                    result = min(result, calculate(1, start, mid) + calculate(1, mid + 1, end))
+                    res = min(res, calculate(1, start, mid) + calculate(1, mid + 1, end))
 
-        dp[flag][start][end] = result
-        return result
-
-    n = len(arr) // 2
-    dp = [[[None for _ in range(n + 1)] for _ in range(n + 1)] for _ in range(2)]
-    numbers = [int(arr[i]) for i in range(0, len(arr), 2)]
-    operations = [arr[i] for i in range(1, len(arr), 2)]
+        dp[flag][start][end] = res
+        return res
 
     return calculate(0, 0, n)
