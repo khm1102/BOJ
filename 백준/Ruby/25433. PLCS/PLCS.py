6886bae669d1b3c04f03e6c1c9a1b5fbd40f75f2
rlagusals1102 <hmkim112@naver.com>
2024-05-11 22:41:09 +0900

    Update PLCS.py

--- 백준/Ruby/25433. PLCS/PLCS.py
+++ 백준/Ruby/25433. PLCS/PLCS.py
@@ -1,57 +1,26 @@
-from math import sqrt
-
 def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(sqrt(num)) + 1):
-        if num % i == 0:
-            return False
-    return True
+    return num>1 and all(num%i for i in range(2,int(num**0.5)+1))
 
+def solve(s1, s2):
+    d = 0
+    match = [0] * 26
 
-def process_lcs_with_bitset(s1, s2):
-    D = 0
-    Match = [0] * 26
+    for i, j in enumerate(s2):
+        match[ord(j) - ord('a')] |= 1 << i
 
-    for i, char in enumerate(s2):
-        Match[ord(char) - ord('a')] |= 1 << i
+    for j in s1:
+        x = match[ord(j) - ord('a')] | d
+        y = (d << 1) | 1
+        d = x ^ (x & (x - y))
 
-    for char in s1:
-        x = Match[ord(char) - ord('a')] | D
-        y = (D << 1) | 1
+    return bin(d).count('1')
 
-        D = x ^ (x & (x - y))
+s, t, x, y = input(), input(), input(), input()
+s, t = map(lambda z: z.replace(y, ''), (s, t))
+pos_s, pos_t = map(lambda z: z.find(x), (s, t))
 
-    return bin(D).count('1')
+s1, s2 = s.split(x, 1) if pos_s != -1 else (s, "")
+t1, t2 = t.split(x, 1) if pos_t != -1 else (t, "")
 
-
-def main():
-    s = input()
-    t = input()
-    x = input()
-    y = input()
-
-    s = s.replace(y, '')
-    t = t.replace(y, '')
-
-    pos_s = s.find(x)
-    pos_t = t.find(x)
-
-    s1 = s[:pos_s]
-    s2 = s[pos_s + 1:]
-    t1 = t[:pos_t]
-    t2 = t[pos_t + 1:]
-
-    ret1 = process_lcs_with_bitset(s1, t1)
-    ret2 = process_lcs_with_bitset(s2, t2)
-
-    for i in range(ret1 + ret2 + 1, -1, -1):
-        if is_prime(i):
-            print(i)
-            return
-
-    print(-1)
-
-
-if __name__ == "__main__":
-    main()
+ret1, ret2 = map(lambda s, t: solve(s, t), (s1, s2), (t1, t2))
+print(next((i for i in range(ret1 + ret2 + 1, -1, -1) if is_prime(i)), -1))
