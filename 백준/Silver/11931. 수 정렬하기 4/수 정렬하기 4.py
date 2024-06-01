aa59563a97d3d97cf4032842aee861a30e67322d
rlagusals1102 <hmkim112@naver.com>
2024-06-01 13:04:58 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 548 ms, Memory: 215352 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,16 +1,15 @@
 import sys
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    
-    n = int(data[0])
-    a = list(map(int, data[1:n+1]))
 
-    a.sort(reverse=True)
+input = sys.stdin.read
+data = input().split()
 
-    for num in a:
-        print(num)
+n = int(data[0])
+arr = list(map(int, data[1:n+1]))
 
-if __name__ == "__main__":
-    main()
+arr.sort(reverse=True)
+
+for num in arr:
+    print(num)
+
+
