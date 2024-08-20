bc84f197889a9d39da7314e6d723b8237cae99ab
rlagusals1102 <hmkim112@naver.com>
2024-08-20 20:54:29 +0900

    [Platinum V] Title: 요세푸스 문제 3, Time: 168 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Platinum/11025. 요세푸스 문제 3/요세푸스 문제 3.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split())
+
+if k==1: print(n)
+else:
+    a,b = 0, 1
+    
+    for _ in range(n):
+        x=(b-a-1)//(k-1)+1
+        if b+x>n:
+            a+=(n-b)*k
+            break
+        a=(a+k*x)%(b+x)
+        b+=x
+
+    print(a+1)
