851a19b8671b13b21ffadd3927ff57eea3ffc54e
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:59:44 +0900

    [Gold IV] Title: 강의실 배정, Time: 420 ms, Memory: 138236 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,26 +1,9 @@
-import heapq
+from heapq import heappush, heapreplace
 
-def min_rooms(sch):
-    if not sch:
-        return 0
+def solve(l:list):
+    l.sort();h =[]
 
-    sch.sort(key=lambda x: x[0])
-    rooms = []
+    for i,j in l: heapreplace(h, j) if h and h[0] <= i else heappush(h,j)
+    return len(h)
 
-    heapq.heappush(rooms, sch[0][1])
-
-    for i in range(1, len(sch)):
-        if sch[i][0] >= rooms[0]:
-            heapq.heappop(rooms)
-        heapq.heappush(rooms, sch[i][1])
-
-    return len(rooms)
-
-N = int(input())
-sch = []
-for _ in range(N):
-    S, T = map(int, input().split())
-    sch.append((S, T))
-
-result = min_rooms(sch)
-print(result)
+print(solve([list(map(int,input().split())) for _ in range(int(input()))]))
