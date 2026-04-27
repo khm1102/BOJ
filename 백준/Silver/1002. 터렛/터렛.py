de517e38f31917de281b26e8ddc640fab6330b4e
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:08:22 +0900

    [Silver III] Title: 터렛, Time: 60 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,13 +1,17 @@
+import math
+
 n = int(input())
 
 for _ in range(n):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5  
-    if distance == 0 and r1 == r2:
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 두 원의 거리 원의방정식
+    if distance == 0 and r1 == r2 :  
         print(-1)
-    elif abs(r1 - r2) == distance or r1 + r2 == distance:
+    elif abs(r1-r2) == distance or r1 + r2 == distance:  
         print(1)
-    elif abs(r1 - r2) < distance < (r1 + r2):
+    elif abs(r1-r2) < distance < (r1+r2) :  
         print(2)
     else:
-        print(0)
+        print(0) 
+        
+# 원의 방정식을 여기서 사용해 보네
