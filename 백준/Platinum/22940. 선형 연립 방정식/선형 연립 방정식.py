048a324b8cae9489cbe7acea4a237a73c884eb97
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:48:31 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
@@ -1,8 +1,1 @@
-l=[list(map(float,input().split()))for _ in range(int(input()))]
-for i in range(len(l)):
-    for j in range(len(l)):
-        if i!=j:
-            r=l[j][i]/l[i][i]
-            for k in range(len(l[0])):l[j][k]-=r*l[i][k]
-    l[i]=[x/l[i][i]for x in l[i]]
-print(*[f'{r[-1]:.0f}'for r in l])
+exec("l=[list(map(float,input().split()))for _ in range(int(input()))];[exec('for j in range(len(l)):\\n if i!=j:\\n  r=l[j][i]/l[i][i]\\n  for k in range(len(l[0])):l[j][k]-=r*l[i][k]\\nl[i]=[x/l[i][i]for x in l[i]]', {'l': l, 'i': i}) for i in range(len(l))];print(*[f'{r[-1]:.0f}'for r in l])")
