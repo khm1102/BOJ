795f896bfa55f538e3ef3548e2ae51eae67e62c3
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:03:55 +0900

    [Gold II] Title: Function, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/24059. Function/Function.py
+++ 백준/Gold/24059. Function/Function.py
@@ -1,42 +1,9 @@
-import sys
-import os
-import math
-import cmath
-import decimal
-import itertools
-import heapq
-from bisect import bisect_left, bisect_right
-from collections import deque
+n = int(input())
+li = list(map(int, input().split()))
+k = int(input())
 
-
-def input(): return sys.stdin.readline().strip()
-
-
-def print(val): return sys.stdout.write(str(val))
-
-
-def main():
-    n = int(input())
-    li = list(map(int, input().split()))
-    k = int(input())
-
-    if n == 0:
-        print(li[0] % k)
-    elif n == 1:
-        answer = li[0]
-        answer = pow(li[1], answer, k)
-        print(answer)
-    else:
-        temp = li[0]
-        temp = pow(li[1], temp, k - 1)
-        temp += k - 1
-
-        answer = li[2]
-        answer = pow(answer, temp, k)
-
-        print(answer)
-
-
-if __name__ == "__main__":
-    main()
-
+f = (lambda n, li, m: 
+                    (li[0] % m) if n == 0 else
+                    (pow(li[1], li[0], m) if n == 1 else
+                    (pow(li[2], pow(li[1], li[0], m - 1) + m - 1, m))))
+print(f(n, li, k))
