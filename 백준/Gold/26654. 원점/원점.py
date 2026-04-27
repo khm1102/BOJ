a4bf54f590cb57080a63d3e50e390a6a601c5917
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:04:04 +0900

    [Gold I] Title: 원점, Time: 1348 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/26654. 원점/원점.py
+++ 백준/Gold/26654. 원점/원점.py
@@ -1,17 +1,9 @@
 import math
-
-def max_verts(N, A, B):
-    a = math.sqrt(2 * A / (N * math.sin(2 * math.pi / N)))
-    b = math.sqrt(B / math.pi)
-
-    if a <= b:
-        return N
-    else:
-        K = int(N * math.asin(b / a) / math.pi) + 1
-        return K
-
-T = int(input())
-for _ in range(T):
-    N, A, B = map(int, input().split())
-    result = max_verts(N, A, B)
-    print(result)
+def f(N,A,B):
+    a=math.sqrt(2*A/(N*math.sin(2 * math.pi / N)))
+    b=math.sqrt(B/math.pi)
+    if a<=b:return N
+    else:return int(N*math.asin(b/a)/math.pi)+1
+for _ in range(int(input())):
+    N,A,B=map(int,input().split())
+    print(f(N,A,B))
