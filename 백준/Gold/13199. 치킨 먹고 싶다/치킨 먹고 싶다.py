415574778fe9c1d22fda7877e332eeeb423c6a60
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:02:32 +0900

    [Gold III] Title: 치킨 먹고 싶다, Time: 140 ms, Memory: 111372 KB -BaekjoonHub

--- 백준/Gold/13199. 치킨 먹고 싶다/치킨 먹고 싶다.py
+++ 백준/Gold/13199. 치킨 먹고 싶다/치킨 먹고 싶다.py
@@ -1,19 +1,13 @@
 import sys
+def input(): return sys.stdin.readline().strip()
+def print(s):return sys.stdout.write(s)
 
-def main():
-    t = int(sys.stdin.readline().strip())
+for _ in range(int(input())):
+    p, m, f, c = map(int,input().split())
 
-    for _ in range(t):
-        p, m, f, c = map(int, sys.stdin.readline().split())
-
-        d = 0
-        n = (m // p) + ((m // p) * c) // f
-        d = (m // p)
-        res = (m // p) * c
-        if res >= f:
-            d += (res - f) // (f - c) + 1
-        
-        sys.stdout.write(str(d - n) + "\n")
-
-if __name__ == "__main__":
-    main()
+    n = (m // p) + ((m // p) * c) // f
+    d = (m // p)
+    res = (m // p) * c
+    if res >= f:
+        d += (res - f) // (f - c) + 1
+    print(str(d - n) + "\n")
