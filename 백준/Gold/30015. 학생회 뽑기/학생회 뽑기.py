55de9aa535be993c4b2e93c370f033644b053303
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:05:15 +0900

    [Gold III] Title: 학생회 뽑기, Time: 356 ms, Memory: 54036 KB -BaekjoonHub

--- 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.py
+++ 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.py
@@ -1,18 +1,20 @@
-def maximize_ability(N, K, A):
-    max_ability = 0
-    for bit in range(20, -1, -1):
-        count = 0
-        new_max_ability = max_ability | (1 << bit)
-        
-        for i in range(N):
-            if (A[i] & new_max_ability) == new_max_ability:
-                count += 1
-        
-        if count >= K:
-            max_ability = new_max_ability
-    
-    return max_ability
+def solve(n, k, arr):
+    ability = 0
 
-N, K = map(int, input().split())
-A = list(map(int, input().split()))
-print(maximize_ability(N, K, A))
+    for i in range(20, -1, -1):
+        cnt = 0
+        new_ability = ability | (1 << i)
+
+        for i in range(n):
+            if (arr[i] & new_ability) == new_ability:
+                cnt += 1
+
+        if cnt >= k:
+            ability = new_ability
+
+    return ability
+
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+print(solve(n, k, arr))
