04ed0fdbe0b52ebbafd77ef84d90aeef08428467
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:41:54 +0900

    [Platinum III] Title: 가장 먼 두 점, Time: 4280 ms, Memory: 44580 KB -BaekjoonHub

---
+++ 백준/Platinum/2049. 가장 먼 두 점/가장 먼 두 점.py
@@ -0,0 +1,38 @@
+def dist_squared(p1, p2):
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+
+def cross_product(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+def convex_hull(points):
+    points.sort()
+    
+    lower_hull = []
+    upper_hull = []
+    
+    for p in points:
+        while len(lower_hull) >= 2 and cross_product(lower_hull[-2], lower_hull[-1], p) <= 0:
+            lower_hull.pop()
+        lower_hull.append(p)
+        
+    for p in reversed(points):
+        while len(upper_hull) >= 2 and cross_product(upper_hull[-2], upper_hull[-1], p) <= 0:
+            upper_hull.pop()
+        upper_hull.append(p)
+    
+    return lower_hull[:-1] + upper_hull[:-1]
+
+n = int(input())
+points = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+convex_points = convex_hull(points)
+max_dist_sq = 0
+
+for i in range(len(convex_points)):
+    for j in range(i + 1, len(convex_points)):
+        max_dist_sq = max(max_dist_sq, dist_squared(convex_points[i], convex_points[j]))
+
+print(max_dist_sq)
