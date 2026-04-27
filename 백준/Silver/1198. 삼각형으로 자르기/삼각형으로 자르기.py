f99e35d6b0a7d65f877a70b89c134119d57a5c85
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:34:40 +0900

    [Silver II] Title: 삼각형으로 자르기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1198. 삼각형으로 자르기/삼각형으로 자르기.py
+++ 백준/Silver/1198. 삼각형으로 자르기/삼각형으로 자르기.py
@@ -1,2 +1,2 @@
 p=[[*map(int,input().split())]for _ in range(int(input()))]
-print(max(a*(d-f)+c*(f-b)+e*(b-d)for a,b in p for c,d in p for e,f in p)/2)
+print(max(a*d+c*f+e*b-c*b-e*d-a*f for a,b in p for c,d in p for e,f in p)/2)
