363650710c5e67667756de33439e1c20501e6f8e
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:35:17 +0900

    [Silver III] Title: 시리얼 번호, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -1,7 +1,4 @@
-def k_sum(s):
-    return sum(int(d) for d in s if d.isdigit())
-
-n = int(input())
-s = [input().strip() for _ in range(n)]
-s.sort(key=lambda x: (len(x), k_sum(x), x))
-print(*s, sep='\n')
+n=int(input())
+s=[input().strip()for _ in range(n)]
+s.sort(key=lambda x:(len(x),sum(int(d)for d in x if d.isdigit()),x))
+print(*s,sep='\n')
