ae2985d6e6ce7d23a5d03bf7f2379555e4064359
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:35:40 +0900

    [Silver III] Title: 정사각형, Time: 264 ms, Memory: 116260 KB -BaekjoonHub

--- 백준/Silver/1485. 정사각형/정사각형.py
+++ 백준/Silver/1485. 정사각형/정사각형.py
@@ -1,17 +1,31 @@
-tc = int(input())
+class Rectangle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.s = []
 
-for _ in range(tc):
-    x = []
-    y = []
-    for i in range(4):
-        x_i, y_i = map(int, input().split())
-        x.append(x_i)
-        y.append(y_i)
+    def calculate_sides(self):
+        for i in range(4):
+            for j in range(i + 1, 4):
+                self.s.append((self.x[i] - self.x[j]) ** 2 + (self.y[i] - self.y[j]) ** 2)
 
-    s = []
-    for i in range(4):
-        for j in range(i + 1, 4):
-            s.append((x[i] - x[j]) ** 2 + (y[i] - y[j]) ** 2)
+        self.s.sort()
 
-    s.sort()
-    print(int(s[0] == s[1] == s[2] == s[3] and s[4] == s[5]))
+    def is_rectangle(self):
+        return int(self.s[0] == self.s[1] == self.s[2] == self.s[3] and self.s[4] == self.s[5])
+
+
+if __name__ == '__main__':
+    tc = int(input())
+
+    for _ in range(tc):
+        x = []
+        y = []
+        for i in range(4):
+            x_i, y_i = map(int, input().split())
+            x.append(x_i)
+            y.append(y_i)
+
+        rectangle = Rectangle(x, y)
+        rectangle.calculate_sides()
+        print(rectangle.is_rectangle())
