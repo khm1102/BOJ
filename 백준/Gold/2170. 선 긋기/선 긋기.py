891c43bf8ecb334a22611b309b712a4c052aff5d
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:55:56 +0900

    [Gold V] Title: 선 긋기, Time: 3640 ms, Memory: 321380 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -1,9 +1,2 @@
-n=int(input())
-a=sorted([list(map(int,input().split()))for _ in range(n)])
-s,e=a[0]
-r=0
-for x,y in a[1:]:
-    if x<=e:e=max(e,y)
-    else:r+=e-s;s,e=x,y
-r+=e-s
-print(r)
+f=-1e9
+print(sum(y-max(x,f)+(f:=y)*0for x,y in sorted([*map(int,i.split())]for i in[*open(0)][1:])if y>f))
