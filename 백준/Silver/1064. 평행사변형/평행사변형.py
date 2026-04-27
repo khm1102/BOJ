d163d3646033fd3290371c63ac75c83f3bd67dc2
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:32:51 +0900

    [Silver IV] Title: 평행사변형, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.py
+++ 백준/Silver/1064. 평행사변형/평행사변형.py
@@ -1,10 +1,10 @@
-calculate_distance = lambda x1, y1, x2, y2: ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-find_triangle_max_diff = lambda ax, ay, bx, by, cx, cy: -1.0 if ((ax - bx) * (ay - cy) == (ay - by) * (ax - cx)) else 2 * (max(
-    [calculate_distance(ax, ay, bx, by) + calculate_distance(ax, ay, cx, cy),
-     calculate_distance(ax, ay, bx, by) + calculate_distance(bx, by, cx, cy),
-     calculate_distance(ax, ay, cx, cy) + calculate_distance(bx, by, cx, cy)]) - min(
-    [calculate_distance(ax, ay, bx, by) + calculate_distance(ax, ay, cx, cy),
-     calculate_distance(ax, ay, bx, by) + calculate_distance(bx, by, cx, cy),
-     calculate_distance(ax, ay, cx, cy) + calculate_distance(bx, by, cx, cy)]))
+dist = lambda x1, y1, x2, y2: ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+solve = lambda ax, ay, bx, by, cx, cy: -1.0 if ((ax - bx) * (ay - cy) == (ay - by) * (ax - cx)) else 2 * (max(
+    [dist(ax, ay, bx, by) + dist(ax, ay, cx, cy),
+     dist(ax, ay, bx, by) + dist(bx, by, cx, cy),
+     dist(ax, ay, cx, cy) + dist(bx, by, cx, cy)]) - min(
+    [dist(ax, ay, bx, by) + dist(ax, ay, cx, cy),
+     dist(ax, ay, bx, by) + dist(bx, by, cx, cy),
+     dist(ax, ay, cx, cy) + dist(bx, by, cx, cy)]))
 ax, ay, bx, by, cx, cy = map(int, input().split())
-print(find_triangle_max_diff(ax, ay, bx, by, cx, cy))
+print(solve(ax, ay, bx, by, cx, cy))
