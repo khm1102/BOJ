3fe4d1e2e3343adff88d0dcb351cd2a339034de9
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:32:09 +0900

    [Diamond V] Title: 헬기착륙장, Time: 280 ms, Memory: 115392 KB -BaekjoonHub

--- 백준/Diamond/2626. 헬기착륙장/헬기착륙장.py
+++ 백준/Diamond/2626. 헬기착륙장/헬기착륙장.py
@@ -1,50 +1,14 @@
 import math
-
-class Point:
-    def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
-
-def input_points():
-    n = int(input())
-    points = []
-    for _ in range(n):
-        x, y = map(int, input().split())
-        points.append(Point(x, y))
-    return points
-
-def solve(points):
-    norm = lambda x, y: x*x + y*y
-    avg = Point()
-    for point in points:
-        avg.x += point.x
-        avg.y += point.y
-    avg.x *= 1.0 / len(points)
-    avg.y *= 1.0 / len(points)
-    ans = 1e10
-    px, py, w = avg.x, avg.y, 0.1
-    cnt = 10000
-    while cnt > 0:
-        max_v = 0
-        max_id = 0
-        for i, point in enumerate(points):
-            tmp_v = norm(px - point.x, py - point.y)
-            if tmp_v > max_v:
-                max_v = tmp_v
-                max_id = i
-        if max_v < ans:
-            ans = max_v
-        px += (points[max_id].x - px) * w
-        py += (points[max_id].y - py) * w
-        w *= 0.995
-        cnt -= 1
-    return px, py, math.sqrt(ans)
-
-def main():
-    points = input_points()
-    px, py, ans = solve(points)
-    print(f"{px:.3f} {py:.3f}")
-    print(f"{ans:.3f}")
-
-if __name__ == "__main__":
-    main()
+n = int(input())
+p = [tuple(map(int, input().split())) for _ in range(n)]
+x = sum(point[0] for point in p) / n
+y = sum(point[1] for point in p) / n
+a, b, w, s = x, y, 0.1, float('inf')
+o = lambda x, y: x ** 2 + y ** 2
+for _ in range(10000):
+    m,v=max(((i, o(a - x, b - y)) for i,(x, y) in enumerate(p)), key=lambda x:x[1])
+    if v<s:s=v
+    a+=(p[m][0]-a)*w
+    b+=(p[m][1]-b)*w
+    w*=0.995
+print(f"{a:.3f} {b:.3f}\n{math.sqrt(s):.3f}")
