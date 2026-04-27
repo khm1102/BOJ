fa8f3eeff53c16e38a2b5126996741149fcb70c2
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:40:24 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 132 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,19 +1,19 @@
 from collections import deque
-import sys
 
-def burst_balloons(balloons):
-    result = []
-    balloons = deque(balloons)
-    while balloons:
-        result.append(balloons[0][1])
-        move = balloons.popleft()[0]
-        if move > 0:
-            balloons.rotate(-move + 1)
+def solve(arr):
+    res = []
+    arr = deque(arr)
+    while arr:
+        res.append(arr[0][1])
+        temp = arr.popleft()[0]
+        
+        if temp > 0:
+            arr.rotate(-temp + 1)
         else:
-            balloons.rotate(-move)
-    return result
+            arr.rotate(-temp)
+    return res
 
 n = int(input())
 arr = list(map(int, input().split()))
 arr = [(arr[i], i + 1) for i in range(n)]
-print(*burst_balloons(arr))
+print(*solve(arr))
