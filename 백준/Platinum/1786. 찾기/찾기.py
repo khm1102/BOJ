51035c030057a79d60daedc47b00f3b259f2f24e
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:41:19 +0900

    [Platinum V] Title: 찾기, Time: 244 ms, Memory: 132220 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -1,53 +1,39 @@
-
-def compute_lps_array(pattern):
-    length = 0
-    i = 1
-    lps = [0] * len(pattern)
-
-    while i < len(pattern):
-        if pattern[i] == pattern[length]:
-            length += 1
-            lps[i] = length
-            i += 1
-        else:
-            if length != 0:
-                length = lps[length - 1]
-            else:
-                lps[i] = 0
-                i += 1
-    return lps
-
-
-def kmp(text, pattern):
-    pos = []
-    m = len(pattern)
-    n = len(text)
-    lps = compute_lps_array(pattern)
-    i, j = 0, 0
-
-    while i < n:
-        if pattern[j] == text[i]:
-            i += 1
+def failfun(p):
+    fail = [0] * len(p)
+    j = 0
+    for i in range(1, len(p)):
+        while j > 0 and p[i] != p[j]:
+            j = fail[j - 1]
+        if p[i] == p[j]:
             j += 1
+            fail[i] = j
+    return fail
 
-        if j == m:
-            pos.append(i - j + 1)
-            j = lps[j - 1]
 
-        elif i < n and pattern[j] != text[i]:
-            if j != 0:
-                j = lps[j - 1]
+def kmp(s, p):
+    fail = failfun(p)
+    cnt = 0
+    q = []
+    j = 0
+    for i in range(len(s)):
+        while j > 0 and s[i] != p[j]:
+            j = fail[j - 1]
+        if s[i] == p[j]:
+            if j == len(p) - 1:
+                q.append(i - len(p) + 2)
+                cnt += 1
+                j = fail[j]
             else:
-                i += 1
+                j += 1
+    return cnt, q
 
-    return pos
 
-def main():
-	t = str(input())
-	p = str(input())
-	res = kmp(t, p)
-	
-	print(len(res), '\n'.join(map(str, res)), sep='\n')
+if __name__ == "__main__":
+    s = input()
+    p = input()
 
-if __name__ == '__main__':
-    main()
+    cnt, positions = kmp(s, p)
+
+    print(cnt)
+    for pos in positions:
+        print(pos, end=" ")
