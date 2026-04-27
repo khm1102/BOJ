5595ede9ca7e100f720b825e3c92259250b92cb6
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:34:20 +0900

    [Silver V] Title: 분수찾기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,17 +1,5 @@
-n = int(input())
-
-line = 0
-end = 0
-while n > end:
-    line += 1
-    end += line
-
-df = end - n
-if line%2 == 0: 
-    top = line - df
-    bt = df + 1
-else:
-    top = df + 1 
-    bt = line - df
-
-print("%d/%d"%(top,bt))
+n=int(input());i=1
+while n>i:n-=i;i+=1
+a=n;b=i-n+1
+if i%2:a,b=b,a
+print(f"{a}/{b}")
