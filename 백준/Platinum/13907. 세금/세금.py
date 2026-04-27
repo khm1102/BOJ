b8f65a6ee813019cb5ec417c3a6bc48f3877bc57
김현민 <rlagusals1102@gmail.com>
2026-04-28 00:46:57 +0900

    [Platinum IV] Title: 세금, Time: 7772 ms, Memory: 49984 KB -BaekjoonHub

--- 백준/Platinum/13907. 세금/세금.py
+++ 백준/Platinum/13907. 세금/세금.py
@@ -1,62 +1,62 @@
 import heapq
 import sys
 
-class N:
+class Node:
     def __init__(self, v, w, c):
         self.v = v
         self.w = w
         self.c = c
     
-    def __lt__(self, o):
-        return self.w < o.w
+    def __lt__(self, other):
+        return self.w < other.w
 
-n, m, k = map(int, sys.stdin.readline().split())
-s, d = map(int, sys.stdin.readline().split())
+N, M, K = map(int, input().split())
+S, D = map(int, input().split())
 
-a = [[] for _ in range(n + 1)]
-dp = [[float('inf')] * n for _ in range(n + 1)]
+arr = [[] for _ in range(N + 1)]
+dp = [[float('inf')] * N for _ in range(N + 1)]
 
-for _ in range(m):
-    x, y, w = map(int, sys.stdin.readline().split())
-    a[x].append(N(y, w, 0))
-    a[y].append(N(x, w, 0))
+for _ in range(M):
+    a, b, w = map(int, input().split())
+    arr[a].append(Node(b, w, 0))
+    arr[b].append(Node(a, w, 0))
 
-for i in range(1, n + 1):
-    dp[i] = [float('inf')] * n
+for i in range(1, N + 1):
+    dp[i] = [float('inf')] * N
 
-h = []
-heapq.heappush(h, N(s, 0, 0))
+heap = []
+heapq.heappush(heap, Node(S, 0, 0))
 
-while h:
-    t = heapq.heappop(h)
-    if t.c + 1 == n:
+while heap:
+    temp = heapq.heappop(heap)
+    if temp.c + 1 == N:
         continue
-    if t.w > dp[t.v][t.c]:
+    if temp.w > dp[temp.v][temp.c]:
         continue
-    for nx in a[t.v]:
-        for i in range(1, t.c + 1):
-            if dp[nx.v][i] < t.w + nx.w:
+    for next_node in arr[temp.v]:
+        for i in range(1, temp.c + 1):
+            if dp[next_node.v][i] < temp.w + next_node.w:
                 break
         else:
-            if dp[nx.v][t.c + 1] > t.w + nx.w:
-                dp[nx.v][t.c + 1] = t.w + nx.w
-                heapq.heappush(h, N(nx.v, t.w + nx.w, t.c + 1))
+            if dp[next_node.v][temp.c + 1] > temp.w + next_node.w:
+                dp[next_node.v][temp.c + 1] = temp.w + next_node.w
+                heapq.heappush(heap, Node(next_node.v, temp.w + next_node.w, temp.c + 1))
 
-r = float('inf')
-for i in range(1, n):
-    r = min(r, dp[d][i])
+result = float('inf')
+for i in range(1, N):
+    result = min(result, dp[D][i])
 
-sys.stdout.write(str(r) + '\n')
+print(result)
 
-def t(p):
-    r = float('inf')
-    for i in range(1, n):
-        if dp[d][i] == float('inf'):
+def tax(p):
+    result = float('inf')
+    for i in range(1, N):
+        if dp[D][i] == float('inf'):
             continue
-        dp[d][i] += i * p
-        r = min(r, dp[d][i])
-    return r
+        dp[D][i] += i * p
+        result = min(result, dp[D][i])
+    return result
 
-for _ in range(k):
-    p = int(sys.stdin.readline())
-    sys.stdout.write(str(t(p)) + '\n')
+for _ in range(K):
+    p = int(input())
+    print(tax(p))
