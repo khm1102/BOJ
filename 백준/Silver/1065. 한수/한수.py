413ae7154f00f3a7ff6b9f3529dfd6c642e61dc4
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:33:10 +0900

    [Silver IV] Title: 한수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,12 +1,18 @@
-n = int(input())
-count = 0
+def is_han(num):
+    # 입력받은 수가 한수인지 검사하는 함수
+    if num < 100:
+        return True  # 100 미만의 수는 모두 한수이므로 True 반환
+    else:
+        nums = list(map(int, str(num)))  # 입력받은 수의 각 자리수를 리스트에 저장
+        diff = nums[1] - nums[0]  # 두 자리수의 차이를 저장할 변수
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] != diff:  # 두 자리수의 차이가 일정하지 않으면
+                return False  # 한수가 아니므로 False 반환
+        return True  # 위 조건을 모두 통과하면 한수이므로 True 반환
 
-if n < 100:  # 1부터 99까지는 모두 한수이므로 그대로 출력
-    print(n)
-else:
-    count = 99  # 1부터 99까지는 모두 한수이므로 먼저 count를 99로 초기화
-    for i in range(100, n + 1):  # 100부터 n까지 한수인지 체크
-        nums = list(map(int, str(i)))  # 각 자리 수를 리스트로 저장
-        if nums[0] - nums[1] == nums[1] - nums[2]:  # 등차수열인지 확인
-            count += 1
-    print(count)
+n = int(input())
+cnt = 0  # 한수의 개수를 세는 변수
+for i in range(1, n+1):
+    if is_han(i):
+        cnt += 1
+print(cnt)
