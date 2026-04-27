9a470259dfeee7bd0e3e0cd66d6953af6a494c13
김현민 <rlagusals1102@gmail.com>
2026-04-28 01:33:59 +0900

    [Silver V] Title: 단어 정렬, Time: 216 ms, Memory: 116960 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,1 @@
-n = int(input())
-lst = []
-
-for i in range(n):
-    lst.append(input())
-set_lst = set(lst)	
-lst = list(set_lst)	
-lst.sort()
-lst.sort(key = len)
-
-for i in lst:
-    print(i)
+print(*sorted({input() for _ in range(int(input()))}, key=lambda x: (len(x), x)), sep='\n')
